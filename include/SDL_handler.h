938a0280d30d2310986c9947f86b5c2ed7237a34
jordi-lete <jordismith20@hotmail.com>
2025-04-09 22:22:18 +0100

    Set up piece rendering

--- include/SDL_handler.h
+++ include/SDL_handler.h
@@ -1,11 +1,23 @@
 #pragma once
 #include <SDL3/SDL.h>
+#include <SDL3_image/SDL_image.h>
 
 class SDL_handler
 {
+private:
+	SDL_Texture* whitePawn, * whiteRook, * whiteKnight, * whiteBishop, * whiteQueen, * whiteKing;
+	SDL_Texture* blackPawn, * blackRook, * blackKnight, * blackBishop, * blackQueen, * blackKing;
+
+	void loadPieceTextures();
+
+	SDL_Texture* loadImage(std::string imageFile);
+
 public:
 	const int SCREEN_WIDTH = 640;
 	const int SCREEN_HEIGHT = 640;
+
+	const float squareWidth = (float)SCREEN_WIDTH / 8.0f;
+	const float squareHeight = (float)SCREEN_HEIGHT / 8.0f;
 
 	SDL_Window* window;
 
@@ -25,6 +37,8 @@ public:
 	bool init();
 
 	void renderBoard();
+
+	void renderPiece(SDL_Texture* piece, int file, int rank);
 
 	void render();
 
--- src/SDL_handler.cpp
+++ src/SDL_handler.cpp
@@ -42,12 +42,28 @@ bool SDL_handler::init()
 		}
 	}
 
+	loadPieceTextures();
+
 	return true;
 
 }
 
 void SDL_handler::cleanup()
 {
+	// Destroy all piece textures
+	SDL_DestroyTexture(whitePawn);
+	SDL_DestroyTexture(whiteRook);
+	SDL_DestroyTexture(whiteKnight);
+	SDL_DestroyTexture(whiteBishop);
+	SDL_DestroyTexture(whiteQueen);
+	SDL_DestroyTexture(whiteKing);
+
+	SDL_DestroyTexture(blackPawn);
+	SDL_DestroyTexture(blackRook);
+	SDL_DestroyTexture(blackKnight);
+	SDL_DestroyTexture(blackBishop);
+	SDL_DestroyTexture(blackQueen);
+	SDL_DestroyTexture(blackKing);
 
 	SDL_DestroySurface(screenSurface);
 	SDL_DestroyWindow(window);
@@ -56,13 +72,27 @@ void SDL_handler::cleanup()
 
 }
 
+void SDL_handler::loadPieceTextures()
+{
+	whitePawn = loadImage("assets/Chess_plt45.svg");
+	whiteRook = loadImage("assets/Chess_rlt45.svg");
+	whiteKnight = loadImage("assets/Chess_nlt45.svg");
+	whiteBishop = loadImage("assets/Chess_blt45.svg");
+	whiteQueen = loadImage("assets/Chess_qlt45.svg");
+	whiteKing = loadImage("assets/Chess_klt45.svg");
+
+	blackPawn = loadImage("assets/Chess_pdt45.svg");
+	blackRook = loadImage("assets/Chess_rdt45.svg");
+	blackKnight = loadImage("assets/Chess_ndt45.svg");
+	blackBishop = loadImage("assets/Chess_bdt45.svg");
+	blackQueen = loadImage("assets/Chess_qdt45.svg");
+	blackKing = loadImage("assets/Chess_kdt45.svg");
+}
+
 void SDL_handler::renderBoard()
 {
 
 	bool white = true; //First square rendered (top left) is white
-
-	float squareWidth = (float)SCREEN_WIDTH / 8.0f;
-	float squareHeight = (float)SCREEN_HEIGHT / 8.0f;
 
 	for (int i = 0; i < 8; i++)
 	{
@@ -80,6 +110,31 @@ void SDL_handler::renderBoard()
 		}
 		white = !white;
 	}
+
+}
+
+SDL_Texture* SDL_handler::loadImage(std::string imageFile)
+{
+
+	SDL_Surface* image = IMG_Load(imageFile.c_str());
+
+	if (!image)
+	{
+		printf("Could not load image: %s", imageFile);
+	}
+
+	SDL_Texture* pieceTexture = SDL_CreateTextureFromSurface(renderer, image);
+
+	return pieceTexture;
+
+}
+
+void SDL_handler::renderPiece(SDL_Texture* piece, int file, int rank)
+{
+
+	SDL_FRect dest = { file * squareWidth, rank * squareHeight, squareWidth, squareHeight };
+
+	SDL_RenderTexture(renderer, piece, NULL, &dest);
 
 }
 
