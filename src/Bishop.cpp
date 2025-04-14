34c9f484d65bdae2fdc602e946411e3b51d58bdc
jordi-lete <jordismith20@hotmail.com>
2025-04-14 22:34:34 +0100

    Possible moves for bishops and kings calculated

--- src/Bishop.cpp
+++ src/Bishop.cpp
@@ -3,6 +3,147 @@
 std::vector<Square> Bishop::getPossibleMoves(Board& board, int file, int rank)
 {
 	std::vector<Square> moves;
+	bool isWhite = board.getPieceColour(board.getPieceAt(file, rank));
+	int newFile = file;
+	int newRank = rank;
+
+	// Check 4 directions until we find a piece or board edge
+	// First - positive x and y
+	while (true)
+	{
+
+		newFile++;
+		newRank++;
+
+		if (board.isValidPosition(newFile, newRank))
+		{
+			Board::PieceType targetPiece = board.getPieceAt(newFile, newRank);
+			if (targetPiece == board.NONE)
+			{
+				moves.push_back({ newFile, newRank });
+				continue;
+			}
+			else if (board.getPieceColour(targetPiece) != isWhite)
+			{
+				moves.push_back({ newFile, newRank });
+				break;
+			}
+			else
+			{
+				break;
+			}
+		}
+		else
+		{
+			break;
+		}
+
+	}
+
+	newFile = file;
+	newRank = rank;
+
+	//  positive x, negative y
+	while (true)
+	{
+
+		newFile++;
+		newRank--;
+
+		if (board.isValidPosition(newFile, newRank))
+		{
+			Board::PieceType targetPiece = board.getPieceAt(newFile, newRank);
+			if (targetPiece == board.NONE)
+			{
+				moves.push_back({ newFile, newRank });
+				continue;
+			}
+			else if (board.getPieceColour(targetPiece) != isWhite)
+			{
+				moves.push_back({ newFile, newRank });
+				break;
+			}
+			else
+			{
+				break;
+			}
+		}
+		else
+		{
+			break;
+		}
+
+	}
+
+	newFile = file;
+	newRank = rank;
+
+	// negative x, positive y
+	while (true)
+	{
+
+		newFile--;
+		newRank++;
+
+		if (board.isValidPosition(newFile, newRank))
+		{
+			Board::PieceType targetPiece = board.getPieceAt(newFile, newRank);
+			if (targetPiece == board.NONE)
+			{
+				moves.push_back({ newFile, newRank });
+				continue;
+			}
+			else if (board.getPieceColour(targetPiece) != isWhite)
+			{
+				moves.push_back({ newFile, newRank });
+				break;
+			}
+			else
+			{
+				break;
+			}
+		}
+		else
+		{
+			break;
+		}
+
+	}
+
+	newFile = file;
+	newRank = rank;
+
+	// negative x and y
+	while (true)
+	{
+
+		newFile--;
+		newRank--;
+
+		if (board.isValidPosition(newFile, newRank))
+		{
+			Board::PieceType targetPiece = board.getPieceAt(newFile, newRank);
+			if (targetPiece == board.NONE)
+			{
+				moves.push_back({ newFile, newRank });
+				continue;
+			}
+			else if (board.getPieceColour(targetPiece) != isWhite)
+			{
+				moves.push_back({ newFile, newRank });
+				break;
+			}
+			else
+			{
+				break;
+			}
+		}
+		else
+		{
+			break;
+		}
+
+	}
 
 	return moves;
 
--- src/King.cpp
+++ src/King.cpp
@@ -2,7 +2,30 @@
 
 std::vector<Square> King::getPossibleMoves(Board& board, int file, int rank)
 {
+	
 	std::vector<Square> moves;
+	bool isWhite = board.getPieceColour(board.getPieceAt(file, rank));
+
+	const int kingMoves[8][2] = {
+		{-1,-1}, {-1,0}, {-1,1}, {0,-1}, {0,1}, {1,-1}, {1,0}, {1,1}
+	};
+
+	for (int i = 0; i < 8; i++)
+	{
+		int newFile = file + kingMoves[i][0];
+		int newRank = rank + kingMoves[i][1];
+
+		if (board.isValidPosition(newFile, newRank))
+		{
+			Board::PieceType targetPiece = board.getPieceAt(newFile, newRank);
+			if (targetPiece == board.NONE || board.getPieceColour(targetPiece) != isWhite)
+			{
+				moves.push_back({ newFile, newRank });
+			}
+		}
+	}
+
+	// ADD CASTLES
 
 	return moves;
 
